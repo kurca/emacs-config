fc38f32448466206152b413f1ee633b388f11fc8
Marek Kuban <marek.kuban@vnet-services.com>
2015-09-17 19:52:35 +0200

    Set default font
    Signed-off-by: Marek Kuban <marek.kuban@vnet-services.com>

--- personal/preload/config.el
+++ personal/preload/config.el
@@ -9,3 +9,9 @@
 	    (set-frame-height init-frame 47)
 		(set-frame-width  init-frame 135) ))
 
+
+;; set default font, nice monotype fonts
+;; (set-default-font "9x15")
+;; (set-default-font "-unknown-Droid Sans Mono-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1")
+(set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+
