e7da963a232ee6315a078c8de97a8912ffb06d57
Marek Kuban <marek.kuban@vnet-services.com>
2015-09-17 19:52:04 +0200

    Set emacs frame startup size
    Signed-off-by: Marek Kuban <marek.kuban@vnet-services.com>

--- personal/preload/config.el
+++ personal/preload/config.el
@@ -1,3 +1,11 @@
 ;; disable guru-mode and whitespace-mode on startup
 (setq prelude-guru nil)
 (setq prelude-whitespace nil)
+
+
+;; set frame size on startup
+(if (window-at 0 0)
+    (let ((init-frame (window-frame (window-at 0 0))))
+	    (set-frame-height init-frame 47)
+		(set-frame-width  init-frame 135) ))
+
