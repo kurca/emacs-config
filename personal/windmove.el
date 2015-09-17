e4a54b7a961c4a1b570e50c34ecda028a829eddd
Marek Kuban <marek.kuban@vnet-services.com>
2015-09-17 19:47:13 +0200

    Key bindings for windmove
    Signed-off-by: Marek Kuban <marek.kuban@vnet-services.com>

---
+++ personal/windmove.el
@@ -0,0 +1,9 @@
+(global-unset-key [S-up])
+(global-unset-key [S-down])
+(global-unset-key [S-left])
+(global-unset-key [S-right])
+
+(global-set-key [C-s-up] 'windmove-up)
+(global-set-key [C-s-down] 'windmove-down)
+(global-set-key [C-s-left] 'windmove-left)
+(global-set-key [C-s-right] 'windmove-right)
