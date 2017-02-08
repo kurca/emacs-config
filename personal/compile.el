47849543c84a393ed22438cb5e4180336c487528
Marek Kuban <m.kuban@vnet-services.com>
2017-02-08 02:07:29 +0100

    Change compilation buffer behavior
    save all changed buffers don't ask
    compilation will stop scrolling on first error, warnings are ignored
    
    
    Signed-off-by: Marek Kuban <m.kuban@vnet-services.com>

--- personal/compile.el
+++ personal/compile.el
@@ -1,2 +1,9 @@
 (global-set-key [C-f7] 'compile)
 (global-set-key [f7] 'recompile)
+
+;; I'm not scared of saving everything.
+(setq compilation-ask-about-save nil)
+;; Stop on the first error.
+(setq compilation-scroll-output 'first-error)
+;; Don't stop on info or warnings.
+(setq compilation-skip-threshold 2)
